3529ca2fbfea9f97e095e875d14fffa8a71d480e
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-07-31 14:13:24 +0200

    Initial Commit

---
+++ Represental.SUI/Represental_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Represental_SUIApp.swift
+//  Represental.SUI
+//
+//  Created by Авазбек Надырбек уулу on 31.07.25.
+//
+
+import SwiftUI
+
+@main
+struct Represental_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
