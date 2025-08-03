31466c060d93262f4f99d731d01c4ce0d1542ff0
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-08-03 14:19:28 +0200

    Here we learned ImagePicker

---
+++ Represental.SUI/ThirdView.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdView.swift
+//  Represental.SUI
+//
+//  Created by Авазбек Надырбек уулу on 03.08.25.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThirdView()
+}
---
+++ Represental.SUI/UIViewControllerRep/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePciker.swift
+//  Represental.SUI
+//
+//  Created by Авазбек Надырбек уулу on 03.08.25.
+//
+
+import Foundation
